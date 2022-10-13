5756107ff3e5d54f86d9c50560709b81f72941a3
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-10-13 10:12:18 +0200

    дз7, дз8, дз9

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = document.querySelector(".change-color");
+const spanText = document.querySelector(".color");
+
+function handleClick() {
+  let randomHexColor = getRandomHexColor();
+  spanText.textContent = randomHexColor;
+  document.body.style.backgroundColor = randomHexColor;
+}
+
+changeColor.addEventListener("click", handleClick);
