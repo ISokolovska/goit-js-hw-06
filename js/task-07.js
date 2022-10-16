212088605368b614072991395f06f50cbe6a0433
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-10-16 17:50:51 +0200

    дз7

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,6 @@
 const inputText = document.querySelector("#font-size-control");
 const spanText = document.querySelector("#text");
 
-const handleInputRange = (event) => {
-  spanText.style.fontSize = `${event.value}px`;
-};
-
-inputText.addEventListener("input", handleInputRange);
+inputText.addEventListener("input", (event) => {
+  spanText.style.fontSize = `${inputText.value}px`;
+});
